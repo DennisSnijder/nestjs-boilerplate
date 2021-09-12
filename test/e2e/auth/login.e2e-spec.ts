5da159b6db7927739bdb4a92d123dfe5b37d747d
Dennis Snijder <dennissnijder97@gmail.com>
2021-09-12 15:23:12 +0200

    Added additional tests for the auth e2e testing the refresh tokens

--- test/e2e/auth/login.e2e-spec.ts
+++ test/e2e/auth/login.e2e-spec.ts
@@ -29,12 +29,40 @@ describe('Login e2e test', () => {
 
         expect(response.status).toBe(201);
         expect(response.body.access_token !== undefined).toBe(true);
+        expect(response.body.refresh_token !== undefined).toBe(true);
     });
 
     test('Invalid password login', async () => {
         const response = await request.post('/auth/login').send({
             email: 'test@snijder.io',
             password: 'invalid-password',
+        });
+
+        expect(response.status).toBe(401);
+        expect(response.body.error).toBe('Unauthorized');
+    });
+
+    test('Login User & refresh token', async () => {
+        const loginResponse = await request.post('/auth/login').send({
+            email: 'test@snijder.io',
+            password: 'test-password',
+        });
+
+        expect(loginResponse.status).toBe(201);
+        expect(loginResponse.body.refresh_token !== undefined).toBe(true);
+
+        const response = await request.post('/auth/refresh').send({
+            refresh_token: loginResponse.body.refresh_token,
+        });
+
+        expect(response.status).toBe(201);
+        expect(response.body.access_token !== undefined).toBe(true);
+        expect(response.body.refresh_token !== undefined).toBe(true);
+    });
+
+    test('Invalid refresh token', async () => {
+        const response = await request.post('/auth/refresh').send({
+            refresh_token: 'invalid-refresh-token',
         });
 
         expect(response.status).toBe(401);
